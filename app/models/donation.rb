f8ae92aaaba9c940cead6789599a4e59b9990f02
João Almeida <joaorsalmeida@gmail.com>
2015-11-24 20:56:12 -0200

    Institution collect donations
    * Users can inform if the institution collects donations
    * User is notified about the donation collection after the donation

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,5 @@
 class Donation < ActiveRecord::Base
   belongs_to :institution
   LABELS = %w(Roupas Brinquedos Móveis Alimentos)
+  validates :name, :mail, :type_of_donation, :institution, presence: true
 end
--- spec/features/visitor_should_registers_donation_spec.rb
+++ spec/features/visitor_should_registers_donation_spec.rb
@@ -13,9 +13,36 @@ describe 'Visitor should register donation' do
 
     click_on 'Doar'
 
-    expect(page).to have_content 'Doação realizada!'
+    expect(page).to have_content 'Doação registrada com sucesso.'
     expect(page).to have_content 'Sandro'
     expect(page).to have_content 'Roupas'
     expect(page).to have_content 'campus@campuscode.com.br'
+  end
+
+  scenario 'and is notified about the collection' do
+    institution = create(:institution, collect_donations: true)
+
+    visit new_institution_donation_path(institution)
+
+    fill_in 'Nome:', with: 'Sandro'
+    select 'Roupas', from: 'Tipo de Doação:'
+    fill_in 'Telefone:', with: '1198765432'
+    fill_in 'E-Mail:', with: 'campus@campuscode.com.br'
+
+    click_on 'Doar'
+
+    expect(page).to have_content 'Doação registrada com sucesso.'
+    expect(page).to have_content 'A instituição entrará em contato para agendar 
+                                 a coleta da sua doação.'
+  end
+
+  scenario 'incomplete' do
+    institution = create(:institution, collect_donations: true)
+
+    visit new_institution_donation_path(institution)
+
+    click_on 'Doar'
+
+    expect(page).to have_content 'Não foi possível registrar sua doação.'
   end
 end
